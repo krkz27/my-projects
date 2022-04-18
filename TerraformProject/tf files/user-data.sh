c30ab03adc48c050c5237166b09fa48a5e93b468
AhmetKarakuza <ahmet.karakuza@hotmail.com>
2022-04-18 12:03:20 -0400

    token updated

--- TerraformProject/tf files/user-data.sh
+++ TerraformProject/tf files/user-data.sh
@@ -4,6 +4,6 @@ yum install python3 -y
 pip3 install flask
 pip3 install flask_mysql
 yum install git -y
-TOKEN="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"
+TOKEN="XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 cd /home/ec2-user && git clone https://$TOKEN@github.com/krkz27/phonebook.git
 python3 /home/ec2-user/phonebook/phonebook-app.py
